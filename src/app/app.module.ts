4e9edf7f4bad2f365f958834a7f174d0569e02bc
joshuajomiller <joshuajosephmiller@gmail.com>
2018-01-25 00:16:05 +0200

    Change to regular bootstrap and set up main layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -32,7 +30,6 @@ import { PrivacyComponent } from './privacy/privacy.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
