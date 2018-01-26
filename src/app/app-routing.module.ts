aaa7d7a96d7fc817ea485b5238d99c7f69904e6b
joshuajomiller <joshuajosephmiller@gmail.com>
2018-01-26 16:09:07 +0200

    Initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {ContactComponent} from './contact/contact.component';
+import {ProfileComponent} from './profile/profile.component';
+import {AboutComponent} from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', component: HomeComponent},
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
