b5b94b0d921afaa5e9fa45e0fa00eef747710652
joshuajomiller <joshuajosephmiller@gmail.com>
2018-01-26 16:09:38 +0200

    Shell layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  companyName: String = 'Company Name';
+  fullDate: Date = new Date();
+  copyrightYear: number;
+
+  constructor(public route: ActivatedRoute) {
+    this.copyrightYear = this.fullDate.getFullYear();
+  }
 }
