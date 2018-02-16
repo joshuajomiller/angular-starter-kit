352cf7dda4f7e06e73fcb4c838b6d1a5d638eae2
joshuajomiller <joshuajosephmiller@gmail.com>
2018-02-16 15:34:39 +0200

    login social object

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,8 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  socialMethods;
+  constructor() {
+    this.socialMethods = [
+      {
+        name: 'facebook',
+        label: 'Facebook',
+        icon: 'fa-facebook-f',
+        color: '#3B5998'
+      },
+      {
+        name: 'twitter',
+        label: 'Twitter',
+        icon: 'fa-twitter',
+        color: '#55acee'
+      },
+      {
+        name: 'google',
+        label: 'Google',
+        icon: 'fa-google',
+        color: '#dd4b39'
+      },
+      {
+        name: 'linkedin',
+        label: 'LinkedIn',
+        icon: 'fa-linkedin-in',
+        color: '#007bb5'
+      },
+    ];
+  }
   ngOnInit() {
   }
 
